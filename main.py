a709fb8dcac34093cbe356373f8362af7f299b5e
Shantanu-0101 <spanchal112000@gmail.com>
2025-11-06 19:08:08 +0530

    Initial commit

--- main.py
+++ main.py
@@ -26,3 +26,13 @@ def view():
     data = load_data()
 
     return data
+
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id : str):
+    #load all the data
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    return {'error': 'patient is not found'}
